7a36eeec53406b14507f832aec1782414905d17f
Paul Smelser <paul.smelser@appdirect.com>
2017-02-10 11:32:34 -0500

    fixing test

--- src/main/java/org/springframework/social/partnercenter/api/uri/UriProvider.java
+++ src/main/java/org/springframework/social/partnercenter/api/uri/UriProvider.java
@@ -3,18 +3,17 @@ package org.springframework.social.partnercenter.api.uri;
 import org.springframework.web.util.UriComponentsBuilder;
 
 public class UriProvider {
-	public static final UriProvider DEFAULT_URL_PROVIDER = UriProvider.US;
 	public static final UriProvider US = builder()
 			.authority("https://login.windows.net")
 			.partnerServiceApiRoot("https://api.partnercenter.microsoft.com")
 			.resourceUrl("https://graph.windows.net")
 			.build();
-
 	public static final UriProvider DE = builder()
 			.authority("https://login.microsoftonline.de")
 			.partnerServiceApiRoot("https://api.partnercenter.microsoft.com")
 			.resourceUrl("https://graph.cloudapi.de")
 			.build();
+	public static final UriProvider DEFAULT_URL_PROVIDER = UriProvider.US;
 
 	private String authority;
 	private String resourceUrl;
