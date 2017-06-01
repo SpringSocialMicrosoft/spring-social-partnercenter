7c48d13a59caff92f1e19beefda845fdb628e835
bigsiri <ismaila.camara@appdirect.com>
2017-06-01 13:12:21 -0400

    PI-9129 : add support of Microsoft service to get subscription provisioning state on PartnerCenterAdmin.

--- src/main/java/org/springframework/social/partnercenter/PartnerCenterAdmin.java
+++ src/main/java/org/springframework/social/partnercenter/PartnerCenterAdmin.java
@@ -2,10 +2,12 @@ package org.springframework.social.partnercenter;
 
 import org.springframework.social.partnercenter.api.customer.AdminCustomerOperations;
 import org.springframework.social.partnercenter.api.customer.user.AdminUserOperations;
+import org.springframework.social.partnercenter.api.order.AdminOrderOperations;
 
 public interface PartnerCenterAdmin extends PartnerCenter {
 	String PROVIDER_ID = "partner-center-admin";
 
 	AdminCustomerOperations getCustomerOperations();
 	AdminUserOperations getUserOperations();
+	AdminOrderOperations getOrderOperations();
 }
