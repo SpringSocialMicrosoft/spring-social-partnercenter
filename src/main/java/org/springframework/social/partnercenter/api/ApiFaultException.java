840c7501602a4489e88d3d6f04f2c1425598a265
Paul Smelser <paul.smelser@gmail.com>
2017-08-09 12:16:53 -0400

    adding information to ApiFaultException i.e. responseHeaders

--- src/main/java/org/springframework/social/partnercenter/api/ApiFaultException.java
+++ src/main/java/org/springframework/social/partnercenter/api/ApiFaultException.java
@@ -2,30 +2,36 @@ package org.springframework.social.partnercenter.api;
 
 import java.util.Map;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.social.partnercenter.serialization.Json;
+import org.springframework.web.client.HttpStatusCodeException;
 
 public class ApiFaultException extends ApiException{
 	private ApiFault payload;
 	private HttpStatus httpStatus;
+	private HttpStatusCodeException cause;
 
-	public ApiFaultException(String message, Throwable cause, HttpStatus status, ApiFault payload) {
+	public ApiFaultException(String message, HttpStatusCodeException cause, HttpStatus status, ApiFault payload) {
 		super(message, cause);
 		this.httpStatus = status;
 		this.payload = payload;
+		this.cause = cause;
 	}
-	public ApiFaultException(String message, Throwable cause, String payload) {
+	public ApiFaultException(String message, HttpStatusCodeException cause, String payload) {
 		super(message, cause);
 		try {
+			this.cause = cause;
 			this.payload = Json.fromJson(payload, ApiFault.class);
 		} catch (Exception e){
 			this.payload = new ApiFault();
 			this.payload.setErrorMessage(payload);
 		}
 	}
-	public ApiFaultException(String message, Throwable cause, HttpStatus status, String payload) {
+	public ApiFaultException(String message, HttpStatusCodeException cause, HttpStatus status, String payload) {
 		super(message, cause);
 		try {
+			this.cause = cause;
 			this.httpStatus = status;
 			this.payload = Json.fromJson(payload, ApiFault.class);
 		} catch (Exception e){
@@ -47,7 +53,20 @@ public class ApiFaultException extends ApiException{
 		return payload.getAttributes();
 	}
 
+	@Deprecated
 	public HttpStatus getHttpStatus() {
 		return httpStatus;
+	}
+
+	public HttpStatus getStatusCode() {
+		return cause.getStatusCode();
+	}
+
+	public String getStatusText() {
+		return cause.getStatusText();
+	}
+
+	public HttpHeaders getResponseHeaders() {
+		return cause.getResponseHeaders();
 	}
 }
