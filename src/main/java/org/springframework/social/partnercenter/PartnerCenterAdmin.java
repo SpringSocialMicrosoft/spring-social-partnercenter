518ec85df298980bbe7c85e8aa3fcb04dc3071ca
Jonathan Fok kan <jonathan.fokkan@appdirect.com>
2017-09-07 13:42:03 -0400

    Added subscription Operations to PartnerCenterAdmin

--- src/main/java/org/springframework/social/partnercenter/PartnerCenterAdmin.java
+++ src/main/java/org/springframework/social/partnercenter/PartnerCenterAdmin.java
@@ -3,6 +3,7 @@ package org.springframework.social.partnercenter;
 import org.springframework.social.partnercenter.api.customer.AdminCustomerOperations;
 import org.springframework.social.partnercenter.api.customer.user.AdminUserOperations;
 import org.springframework.social.partnercenter.api.order.AdminOrderOperations;
+import org.springframework.social.partnercenter.api.order.subscription.AdminSubscriptionOperations;
 
 public interface PartnerCenterAdmin extends PartnerCenter {
 	String PROVIDER_ID = "partner-center-admin";
@@ -10,4 +11,5 @@ public interface PartnerCenterAdmin extends PartnerCenter {
 	AdminCustomerOperations getCustomerOperations();
 	AdminUserOperations getUserOperations();
 	AdminOrderOperations getOrderOperations();
+	AdminSubscriptionOperations getSubscriptionOperations();
 }
