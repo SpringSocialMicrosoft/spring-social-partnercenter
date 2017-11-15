20457720efdd9fe30bee02eed6763a73a7a1f450
Paul Smelser <paul.smelser@gmail.com>
2017-11-15 13:53:08 -0500

    release branch

--- travis-release.sh
+++ travis-release.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
-if [ $TRAVIS_PULL_REQUEST_BRANCH =~ ^release/major/.* ];
+if [[ $TRAVIS_PULL_REQUEST_BRANCH =~ ^release/major/.* ]];
 then
+	echo "Creating Major Release"
 	gradle release -Prelease.disableChecks -Prelease.pushTagsOnly -Prelease.customUsername=$GITHUB_USER -Prelease.customPassword=$GITHUB_PASSWORD -Prelease.versionIncrementer=incrementMajor -s;
-elif [ $TRAVIS_PULL_REQUEST_BRANCH =~ ^release/minor/.* ];
+elif [[ $TRAVIS_PULL_REQUEST_BRANCH =~ ^release/minor/.* ]];
 then
+	echo "Creating Minor Release"
 	gradle release -Prelease.disableChecks -Prelease.pushTagsOnly -Prelease.customUsername=$GITHUB_USER -Prelease.customPassword=$GITHUB_PASSWORD -Prelease.versionIncrementer=incrementMinor -s;
-elif [ $TRAVIS_PULL_REQUEST_BRANCH =~ ^release/.* ];
+elif [[ $TRAVIS_PULL_REQUEST_BRANCH =~ ^release/.* ]];
 then
+	echo "Creating Patch Release"
 	gradle release -Prelease.disableChecks -Prelease.pushTagsOnly -Prelease.customUsername=$GITHUB_USER -Prelease.customPassword=$GITHUB_PASSWORD -Prelease.versionIncrementer=incrementPatch -s;
 fi
