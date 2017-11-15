63b7d1fab120a1b70b9a21b266ee1e294eef56f1
Paul Smelser <paul.smelser@gmail.com>
2017-11-15 11:32:21 -0500

    release branch

--- travis-release.sh
+++ travis-release.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ];
+if [ "$TRAVIS_PULL_REQUEST_BRANCH" =~ ^release/major/.* ];
 then
-	if [ "$TRAVIS_PULL_REQUEST_BRANCH" =~ "^release/major/.*" ];
-	then
-		gradle release -Prelease.disableChecks -Prelease.pushTagsOnly -Prelease.customUsername=$GITHUB_USER -Prelease.customPassword=$GITHUB_PASSWORD -Prelease.versionIncrementer=incrementMajor -s;
-	elif [ "$TRAVIS_PULL_REQUEST_BRANCH" =~ "^release/minor/.*" ];
-	then
-		gradle release -Prelease.disableChecks -Prelease.pushTagsOnly -Prelease.customUsername=$GITHUB_USER -Prelease.customPassword=$GITHUB_PASSWORD -Prelease.versionIncrementer=incrementMinor -s;
-	elif [ "$TRAVIS_PULL_REQUEST_BRANCH" =~ "^release/.*" ];
-	then
-		gradle release -Prelease.disableChecks -Prelease.pushTagsOnly -Prelease.customUsername=$GITHUB_USER -Prelease.customPassword=$GITHUB_PASSWORD -Prelease.versionIncrementer=incrementPatch -s;
-	fi
+	gradle release -Prelease.disableChecks -Prelease.pushTagsOnly -Prelease.customUsername=$GITHUB_USER -Prelease.customPassword=$GITHUB_PASSWORD -Prelease.versionIncrementer=incrementMajor -s;
+elif [ "$TRAVIS_PULL_REQUEST_BRANCH" =~ ^release/minor/.* ];
+then
+	gradle release -Prelease.disableChecks -Prelease.pushTagsOnly -Prelease.customUsername=$GITHUB_USER -Prelease.customPassword=$GITHUB_PASSWORD -Prelease.versionIncrementer=incrementMinor -s;
+elif [ "$TRAVIS_PULL_REQUEST_BRANCH" =~ ^release/.* ];
+then
+	gradle release -Prelease.disableChecks -Prelease.pushTagsOnly -Prelease.customUsername=$GITHUB_USER -Prelease.customPassword=$GITHUB_PASSWORD -Prelease.versionIncrementer=incrementPatch -s;
 fi
