e32149080ba6982efc1624d54a90740319965162
Paul Smelser <paul.smelser@gmail.com>
2017-11-15 14:35:36 -0500

    release branch

--- .travis/scripts/travis-release.sh
+++ .travis/scripts/travis-release.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 
-gradle release -Prelease.disableChecks -Prelease.pushTagsOnly -Prelease.customUsername=$GITHUB_USER -Prelease.customPassword=$GITHUB_PASSWORD -Prelease.versionIncrementer=incrementMinor -s;
-#if [[ $TRAVIS_PULL_REQUEST_BRANCH =~ ^release/major/.* ]];
-#then
-#	echo "Creating Major Release"
-#	gradle release -Prelease.disableChecks -Prelease.pushTagsOnly -Prelease.customUsername=$GITHUB_USER -Prelease.customPassword=$GITHUB_PASSWORD -Prelease.versionIncrementer=incrementMajor -s;
-#elif [[ $TRAVIS_PULL_REQUEST_BRANCH =~ ^release/minor/.* ]];
-#then
-#	echo "Creating Minor Release"
-#	gradle release -Prelease.disableChecks -Prelease.pushTagsOnly -Prelease.customUsername=$GITHUB_USER -Prelease.customPassword=$GITHUB_PASSWORD -Prelease.versionIncrementer=incrementMinor -s;
-#elif [[ $TRAVIS_PULL_REQUEST_BRANCH =~ ^release/.* ]];
-#then
-#	echo "Creating Patch Release"
-#	gradle release -Prelease.disableChecks -Prelease.pushTagsOnly -Prelease.customUsername=$GITHUB_USER -Prelease.customPassword=$GITHUB_PASSWORD -Prelease.versionIncrementer=incrementPatch -s;
-#fi
+if [[ $release_type == "major" ]];
+then
+	echo "Creating Major Release"
+	gradle release -Prelease.disableChecks -Prelease.pushTagsOnly -Prelease.customUsername=$GITHUB_USER -Prelease.customPassword=$GITHUB_PASSWORD -Prelease.versionIncrementer=incrementMajor -s;
+elif [[ $release_type == "minor" ]];
+then
+	echo "Creating Minor Release"
+	gradle release -Prelease.disableChecks -Prelease.pushTagsOnly -Prelease.customUsername=$GITHUB_USER -Prelease.customPassword=$GITHUB_PASSWORD -Prelease.versionIncrementer=incrementMinor -s;
+elif [[ $release_type == "patch" ]];
+then
+	echo "Creating Patch Release"
+	gradle release -Prelease.disableChecks -Prelease.pushTagsOnly -Prelease.customUsername=$GITHUB_USER -Prelease.customPassword=$GITHUB_PASSWORD -Prelease.versionIncrementer=incrementPatch -s;
+fi
