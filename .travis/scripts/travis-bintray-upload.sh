c17b75494cbc9f40f7e519d14a94a72bf89a2974
Paul Smelser <paul.smelser@gmail.com>
2017-12-05 11:44:06 -0500

    Release fix

--- .travis/scripts/travis-bintray-upload.sh
+++ .travis/scripts/travis-bintray-upload.sh
@@ -4,7 +4,7 @@ last_merged_pr_branch=$(git log --oneline --merges master -1 | sed 's/^[^/]*\///
 
 if [[ ${last_merged_pr_branch} =~ ^release/.* ]];
 then
-	gradle bintrayUpload -PbintrayUser=${BINTRAY_USER} -PbintrayKey=${BINTRAY_KEY} -PmavenCentralUser=${MAVEN_USER} -PmavenCentralPassword=${MAVEN_PASSWORD};
+	gradle bintrayUpload -PbintrayUser=$BINTRAY_USER -PbintrayKey=$BINTRAY_KEY -PmavenCentralUser=$MAVEN_USER -PmavenCentralPassword=$MAVEN_PASSWORD;
 else
 	echo "Release pattern not matched. No release will be uploaded to Bintray"
 fi
