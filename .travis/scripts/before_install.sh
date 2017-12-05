075f6fb75cad4ff787ea5a841d730a6e75ae3d78
Paul Smelser <paul.smelser@gmail.com>
2017-12-05 15:38:17 -0500

    permissions

--- .travis/scripts/before_install.sh
+++ .travis/scripts/before_install.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 if [[ "$TRAVIS_PULL_REQUEST" == "false" ]];
 then
 	echo "$key_password" | gpg -o .travis/id_rsa_partner_center.dec --passphrase-fd 0 .travis/id_rsa_partner_center.enc;
