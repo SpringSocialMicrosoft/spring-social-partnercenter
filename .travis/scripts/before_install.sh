cf97f953a3e948029aa1ab359dad4a5bebe63af1
Jonathan Fok kan <jonathan.fokkan@appdirect.com>
2017-12-05 17:55:50 -0500

    Fixed permissions on key file

--- .travis/scripts/before_install.sh
+++ .travis/scripts/before_install.sh
@@ -4,6 +4,7 @@ if [[ "$TRAVIS_PULL_REQUEST" == "false" ]];
 then
     echo "$key_password" | gpg -o .travis/id_rsa_partner_center.dec --passphrase-fd 0 .travis/id_rsa_partner_center.enc;
     eval "$(ssh-agent -s)"
+    chmod 600 .travis/id_rsa_partner_center.dec
     ssh-add .travis/id_rsa_partner_center.dec
     git remote remove origin
     git remote add origin ${REPO}
