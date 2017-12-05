4c1ad1cdb4eb78402189dee2a651395bd0390abd
Paul Smelser <paul.smelser@gmail.com>
2017-12-05 11:33:51 -0500

    Release fix

--- .travis/scripts/travis-release.sh
+++ .travis/scripts/travis-release.sh
@@ -7,15 +7,15 @@ echo ${last_merged_pr_branch}
 if [[ ${last_merged_pr_branch} =~ ^release/major/.* ]];
 then
 	echo "Creating Major Release"
-	gradle release -Prelease.disableChecks -Prelease.pushTagsOnly -Dorg.ajoberstar.grgit.auth.ssh.private=${GITHUB_SSH_KEY} -Dorg.ajoberstar.grgit.auth.session.config.StrictHostKeyChecking=no -Prelease.versionIncrementer=incrementMajor -s;
+	gradle release -Prelease.disableChecks -Prelease.pushTagsOnly -DcustomKey=${GITHUB_SSH_KEY} -Prelease.versionIncrementer=incrementMajor -s;
 elif [[ ${last_merged_pr_branch} =~ ^release/minor/.* ]];
 then
 	echo "Creating Minor Release"
-	gradle release -Prelease.disableChecks -Prelease.pushTagsOnly -Dorg.ajoberstar.grgit.auth.ssh.private=${GITHUB_SSH_KEY} -Dorg.ajoberstar.grgit.auth.session.config.StrictHostKeyChecking=no -Prelease.versionIncrementer=incrementMinor -s;
+	gradle release -Prelease.disableChecks -Prelease.pushTagsOnly -DcustomKey=${GITHUB_SSH_KEY} -Prelease.versionIncrementer=incrementMinor -s;
 elif [[ ${last_merged_pr_branch} =~ ^release/.* ]];
 then
 	echo "Creating Patch Release"
-	gradle release -Prelease.disableChecks -Prelease.pushTagsOnly -Dorg.ajoberstar.grgit.auth.ssh.private=${GITHUB_SSH_KEY} -Dorg.ajoberstar.grgit.auth.session.config.StrictHostKeyChecking=no -Prelease.versionIncrementer=incrementPatch -s;
+	gradle release -Prelease.disableChecks -Prelease.pushTagsOnly -DcustomKey=${GITHUB_SSH_KEY} -Prelease.versionIncrementer=incrementPatch -s;
 else
 	echo "Release Regex not matched. No release will be built."
 fi
