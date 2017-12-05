41c60e0eaf96b0dc6928acf3c642170fc62640f0
Paul Smelser <paul.smelser@gmail.com>
2017-12-05 10:27:40 -0500

    Release fix

--- .travis/scripts/travis-release.sh
+++ .travis/scripts/travis-release.sh
@@ -2,15 +2,15 @@
 
 last_merged_pr_branch=$(git log --oneline --merges master -1 | sed 's/.*\///')
 
-if [[ ${last_merged_pr_branch} =~ ^release/major/.* ]];
+if [[ ${last_merged_pr_branch} =~ .*/release/major/.* ]];
 then
 	echo "Creating Major Release"
 	gradle release -Prelease.disableChecks -Prelease.pushTagsOnly -Prelease.customUsername=$GITHUB_USER -Prelease.customPassword=$GITHUB_PASSWORD -Prelease.versionIncrementer=incrementMajor -s;
-elif [[ ${last_merged_pr_branch} =~ ^release/minor/.* ]];
+elif [[ ${last_merged_pr_branch} =~ .*/release/minor/.* ]];
 then
 	echo "Creating Minor Release"
 	gradle release -Prelease.disableChecks -Prelease.pushTagsOnly -Prelease.customUsername=$GITHUB_USER -Prelease.customPassword=$GITHUB_PASSWORD -Prelease.versionIncrementer=incrementMinor -s;
-elif [[ ${last_merged_pr_branch} =~ ^release/.* ]];
+elif [[ ${last_merged_pr_branch} =~ .*/release/.* ]];
 then
 	echo "Creating Patch Release"
 	gradle release -Prelease.disableChecks -Prelease.pushTagsOnly -Prelease.customUsername=$GITHUB_USER -Prelease.customPassword=$GITHUB_PASSWORD -Prelease.versionIncrementer=incrementPatch -s;
