7d845d97b9ff96e508e56496c1b89d4e9a19c0b7
Paul Smelser <paul.smelser@gmail.com>
2017-12-05 10:52:35 -0500

    Release fix

--- .travis/scripts/travis-release.sh
+++ .travis/scripts/travis-release.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-last_merged_pr_branch=$(git log --oneline --merges master -1 | sed 's/.*\///')
-
-if [[ ${last_merged_pr_branch} =~ .*/release/major/.* ]];
+last_merged_pr_branch=$(git log --oneline --merges master -1 | sed 's/^[^/]*\///')
 
 echo ${last_merged_pr_branch}
+
+if [[ ${last_merged_pr_branch} =~ ^release/major/.* ]];
 then
 	echo "Creating Major Release"
 	gradle release -Prelease.disableChecks -Prelease.pushTagsOnly -Prelease.customUsername=$GITHUB_USER -Prelease.customPassword=$GITHUB_PASSWORD -Prelease.versionIncrementer=incrementMajor -s;
-elif [[ ${last_merged_pr_branch} =~ .*/release/minor/.* ]];
+elif [[ ${last_merged_pr_branch} =~ ^release/minor/.* ]];
 then
 	echo "Creating Minor Release"
 	gradle release -Prelease.disableChecks -Prelease.pushTagsOnly -Prelease.customUsername=$GITHUB_USER -Prelease.customPassword=$GITHUB_PASSWORD -Prelease.versionIncrementer=incrementMinor -s;
-elif [[ ${last_merged_pr_branch} =~ .*/release/.* ]];
+elif [[ ${last_merged_pr_branch} =~ ^release/.* ]];
 then
 	echo "Creating Patch Release"
 	gradle release -Prelease.disableChecks -Prelease.pushTagsOnly -Prelease.customUsername=$GITHUB_USER -Prelease.customPassword=$GITHUB_PASSWORD -Prelease.versionIncrementer=incrementPatch -s;
