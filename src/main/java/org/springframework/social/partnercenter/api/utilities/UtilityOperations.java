5a4073ce1b2b7b6303376d6697de35aee49f206b
Paul Smelser <paul.smelser@gmail.com>
2018-05-07 19:42:48 -0300

    resolves #165 removing one last audit record operation in UtilityOperations. they are now in AuditOperations

--- src/main/java/org/springframework/social/partnercenter/api/utilities/UtilityOperations.java
+++ src/main/java/org/springframework/social/partnercenter/api/utilities/UtilityOperations.java
@@ -1,30 +1,11 @@
 package org.springframework.social.partnercenter.api.utilities;
 
-import java.time.Instant;
-
 import org.springframework.http.ResponseEntity;
-import org.springframework.social.partnercenter.api.PartnerCenterResponse;
-import org.springframework.social.partnercenter.api.audit.AuditRecord;
 import org.springframework.social.partnercenter.api.customer.Address;
-import org.springframework.social.partnercenter.api.customer.query.Filter;
 
 public interface UtilityOperations {
 	ResponseEntity<CountryInformation> getAddressFormattingRulesByMarket(String isoCodeId);
 	Boolean isDomainAvailable(String domainId);
 	ResponseEntity<Boolean> validateAddress(Address address);
 	ResponseEntity deleteCustomer(String customerId);
-
-	/**
-	 * Retrieves audit records for the previous 30 days from the current date, or for a date range specified by including the start date and/or the end date.
-	 * Note, however, that for performance reasons activity log data availability is limited to the previous 90 days. Requests with a start date greater than 90 days
-	 * prior to the current date will receive a bad request exception (error code: 400) and an appropriate message.
-	 *
-	 * This has been replace by {@link org.springframework.social.partnercenter.api.audit.AuditOperations#getPartnerCenterActivity(Instant, Instant)}
-	 * @param startDate Beginning of audit period
-	 * @param endDate End of audit Period
-	 * @return list of activity
-	 * @see <a href="https://msdn.microsoft.com/en-us/library/partnercenter/mt725332.aspx">MSDN: Get a record of Partner Center activity</a>
-	 */
-	@Deprecated
-	ResponseEntity<PartnerCenterResponse<AuditRecord>> getActivityByUser(Instant startDate, Instant endDate);
 }
--- src/main/java/org/springframework/social/partnercenter/api/utilities/impl/UtilityTemplate.java
+++ src/main/java/org/springframework/social/partnercenter/api/utilities/impl/UtilityTemplate.java
@@ -1,26 +1,18 @@
 package org.springframework.social.partnercenter.api.utilities.impl;
 
-import static java.time.ZoneId.of;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.http.ResponseEntity.ok;
-import static org.springframework.social.partnercenter.serialization.Json.toJson;
-import static org.springframework.social.partnercenter.time.PartnerCenterDateTimeFormatter.PARTNER_CENTER_UTC;
 import static org.springframework.util.Assert.notNull;
 
-import java.time.Instant;
 import java.util.Objects;
 
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.social.partnercenter.PartnerCenter;
 import org.springframework.social.partnercenter.api.AbstractTemplate;
 import org.springframework.social.partnercenter.api.ApiFaultException;
-import org.springframework.social.partnercenter.api.PartnerCenterResponse;
-import org.springframework.social.partnercenter.api.audit.AuditRecord;
 import org.springframework.social.partnercenter.api.customer.Address;
-import org.springframework.social.partnercenter.api.customer.query.Filter;
 import org.springframework.social.partnercenter.api.utilities.CountryInformation;
 import org.springframework.social.partnercenter.api.utilities.UtilityOperations;
 import org.springframework.social.partnercenter.http.client.RestResource;
@@ -82,16 +74,5 @@ public class UtilityTemplate extends AbstractTemplate implements UtilityOperatio
 		return restResource.request()
 				.pathSegment("customers", customerId)
 				.delete();
-	}
-
-	@Override
-	public ResponseEntity<PartnerCenterResponse<AuditRecord>> getActivityByUser(Instant startDate, Instant endDate) {
-		notNull(startDate, "[Assertion failed] - startDate argument must be null");
-		notNull(endDate, "[Assertion failed] - endDate argument must be null");
-		return restResource.request()
-				.pathSegment("auditrecords")
-				.queryParam("startDate", startDate.atZone(of("UTC")).format(PARTNER_CENTER_UTC))
-				.queryParam("endDate", endDate.atZone(of("UTC")).format(PARTNER_CENTER_UTC))
-				.get(new ParameterizedTypeReference<PartnerCenterResponse<AuditRecord>>() {});
 	}
 }
