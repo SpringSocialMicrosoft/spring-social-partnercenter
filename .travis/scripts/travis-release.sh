6cfb5e88008c365eb5432219a989e18ca661ba16
Paul Smelser <paul.smelser@gmail.com>
2018-08-22 22:29:07 -0400

    adding the patch releases to the travis release bash script This will allow us to make patch releases on previous versions without having to release manually

--- .travis/scripts/travis-release.sh
+++ .travis/scripts/travis-release.sh
@@ -15,7 +15,16 @@ then
 elif [[ ${last_merged_pr_branch} =~ ^release/.* ]];
 then
 	echo "Creating Patch Release"
-	gradle release -Prelease.disableChecks -Prelease.pushTagsOnly -Prelease.customKeyFile=".travis/id_rsa_partner_center.dec" -Prelease.versionIncrementer=incrementPatch -s;
+	if [[ ${last_merged_pr_branch} =~ ^release/patch/.* ]];
+	then
+		patch_pattern="release/patch/"
+		new_release=${last_merged_pr_branch/release\/patch\/""}
+		gradle release -Prelease.disableChecks -Prelease.pushTagsOnly -Prelease.customKeyFile=".travis/id_rsa_partner_center.dec" -Prelease.version=${new_release} -s
+	else
+		gradle release -Prelease.disableChecks -Prelease.pushTagsOnly -Prelease.customKeyFile=".travis/id_rsa_partner_center.dec" -Prelease.versionIncrementer=incrementPatch -s;
+	fi
 else
 	echo "Release Regex not matched. No release will be built."
 fi
+
+
