f0ea7097cebeb4d161162920926cc7d50071377b
Paul Smelser <paul.smelser@gmail.com>
2018-08-23 07:55:45 -0400

    cleaning up the release bash script a bit

--- .travis/scripts/travis-release.sh
+++ .travis/scripts/travis-release.sh
@@ -1,30 +1,51 @@
 #!/usr/bin/env bash
 
-last_merged_pr_branch=$(git log --oneline --merges master -1 | sed 's/^[^/]*\///')
+function release() {
+	local last_merged_pr_branch=$(git log --oneline --merges master -1 | sed 's/^[^/]*\///')
+	echo ${last_merged_pr_branch}
 
-echo ${last_merged_pr_branch}
+	if [[ ${last_merged_pr_branch} =~ ^release/major/.* ]];
+	then
+		majorRelease
+	elif [[ ${last_merged_pr_branch} =~ ^release/minor/.* ]];
+	then
+		minorRelease
+	elif [[ ${last_merged_pr_branch} =~ ^release/.* ]];
+	then
+		patchRelease ${last_merged_pr_branch}
+	else
+		echo "Release Regex not matched. No release will be built."
+	fi
+}
 
-if [[ ${last_merged_pr_branch} =~ ^release/major/.* ]];
-then
+function majorRelease(){
 	echo "Creating Major Release"
 	gradle release -Prelease.disableChecks -Prelease.pushTagsOnly -Prelease.customKeyFile=".travis/id_rsa_partner_center.dec" -Prelease.versionIncrementer=incrementMajor -s;
-elif [[ ${last_merged_pr_branch} =~ ^release/minor/.* ]];
-then
+}
+
+function minorRelease() {
 	echo "Creating Minor Release"
 	gradle release -Prelease.disableChecks -Prelease.pushTagsOnly -Prelease.customKeyFile=".travis/id_rsa_partner_center.dec" -Prelease.versionIncrementer=incrementMinor -s;
-elif [[ ${last_merged_pr_branch} =~ ^release/.* ]];
-then
+}
+
+function patchRelease() {
 	echo "Creating Patch Release"
-	if [[ ${last_merged_pr_branch} =~ ^release/patch/.* ]];
+	local last_merged_pr_branch=$1
+	if [[ ${last_merged_pr_branch} =~ ^release/patch/[0-9]+.[0-9]+.[0-9]+ ]];
 	then
-		patch_pattern="release/patch/"
-		new_release=${last_merged_pr_branch/release\/patch\/""}
-		gradle release -Prelease.disableChecks -Prelease.pushTagsOnly -Prelease.customKeyFile=".travis/id_rsa_partner_center.dec" -Prelease.version=${new_release} -s
+		versionedRelease ${last_merged_pr_branch}
 	else
 		gradle release -Prelease.disableChecks -Prelease.pushTagsOnly -Prelease.customKeyFile=".travis/id_rsa_partner_center.dec" -Prelease.versionIncrementer=incrementPatch -s;
 	fi
-else
-	echo "Release Regex not matched. No release will be built."
-fi
 
+}
 
+function versionedRelease() {
+	patch_pattern="release/patch/"
+	local last_merged_pr_branch=$1
+	new_release=${last_merged_pr_branch/release\/patch\/""}
+	echo "Forced Version=${new_release}"
+	gradle release -Prelease.disableChecks -Prelease.pushTagsOnly -Prelease.customKeyFile=".travis/id_rsa_partner_center.dec" -Prelease.version=${new_release} -s
+}
+
+release
