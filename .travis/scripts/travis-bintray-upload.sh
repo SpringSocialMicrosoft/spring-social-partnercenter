defd23af4f9d405d7a61f7ed11e5356b104c262b
Paul Smelser <paul.smelser@gmail.com>
2018-09-13 14:38:38 -0400

    cleaning up the release bash script a bit

--- .travis/scripts/travis-bintray-upload.sh
+++ .travis/scripts/travis-bintray-upload.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-last_merged_pr_branch=$(git log --oneline --merges master -1 | sed 's/^[^/]*\///')
+function upload() {
+	local last_merged_pr_branch=$(git log --oneline --merges master -1 | sed 's/^[^/]*\///')
+	if [[ ${last_merged_pr_branch} =~ ^release/.* ]];
+	then
+		gradle bintrayUpload -PbintrayUser=$BINTRAY_USER -PbintrayKey=$BINTRAY_KEY -PmavenCentralUser=$MAVEN_USER -PmavenCentralPassword=$MAVEN_PASSWORD;
+	else
+		echo "Release pattern not matched. No release will be uploaded to Bintray"
+	fi
+}
 
-if [[ ${last_merged_pr_branch} =~ ^release/.* ]];
-then
-	gradle bintrayUpload -PbintrayUser=$BINTRAY_USER -PbintrayKey=$BINTRAY_KEY -PmavenCentralUser=$MAVEN_USER -PmavenCentralPassword=$MAVEN_PASSWORD;
-else
-	echo "Release pattern not matched. No release will be uploaded to Bintray"
-fi
+upload
