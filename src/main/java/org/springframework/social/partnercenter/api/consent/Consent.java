2ca496c0bfe27c40952eb4a658efd1927bad2fd5
Jonathan Fok kan <jonathan.fokkan@appdirect.com>
2018-09-27 14:10:38 -0400

    Added constructor, getters and setters to Consent and ApplicationGrants

--- src/main/java/org/springframework/social/partnercenter/api/consent/Consent.java
+++ src/main/java/org/springframework/social/partnercenter/api/consent/Consent.java
@@ -7,4 +7,34 @@ public class Consent {
     private String displayName;
     private String applicationId;
     private ApplicationGrants applicationGrants;
+
+    public Consent(String displayName, String applicationId, ApplicationGrants applicationGrants) {
+        this.displayName = displayName;
+        this.applicationId = applicationId;
+        this.applicationGrants = applicationGrants;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getApplicationId() {
+        return applicationId;
+    }
+
+    public void setApplicationId(String applicationId) {
+        this.applicationId = applicationId;
+    }
+
+    public ApplicationGrants getApplicationGrants() {
+        return applicationGrants;
+    }
+
+    public void setApplicationGrants(ApplicationGrants applicationGrants) {
+        this.applicationGrants = applicationGrants;
+    }
 }
