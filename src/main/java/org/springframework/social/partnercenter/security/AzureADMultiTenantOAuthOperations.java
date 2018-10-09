342e720a5609dba6969a90091cc9e822f608bc91
Jonathan Fok kan <jonathan.fokkan@appdirect.com>
2018-10-09 17:35:45 -0400

    Extracted static method to interface as default method

--- src/main/java/org/springframework/social/partnercenter/security/AzureADMultiTenantOAuthOperations.java
+++ src/main/java/org/springframework/social/partnercenter/security/AzureADMultiTenantOAuthOperations.java
@@ -1,9 +1,18 @@
 package org.springframework.social.partnercenter.security;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.springframework.social.oauth2.AccessGrant;
 import org.springframework.social.oauth2.OAuth2Parameters;
+import org.springframework.social.partnercenter.connect.PartnerCenterJWT;
 import org.springframework.social.partnercenter.http.logging.LogLevel;
 import org.springframework.util.MultiValueMap;
+
+import com.google.common.base.Charsets;
+import com.google.common.base.Splitter;
 
 public interface AzureADMultiTenantOAuthOperations {
     String buildAuthorizeUrl(String redirectUri, String state);
@@ -19,8 +28,6 @@ public interface AzureADMultiTenantOAuthOperations {
      * @return the access grant.
      */
     String exchangeForRefreshToken(String authorizationCode, String partnerTenantId, String redirectUri, MultiValueMap<String, String> additionalParameters);
-
-    DelegatedAccessGrant extractDelegatedAccessGrant(String body);
 
     /**
      * Exchange the refresh token for an access token
@@ -38,4 +45,23 @@ public interface AzureADMultiTenantOAuthOperations {
     void enableSlf4j(LogLevel logLevel);
 
     boolean isSlf4jEnabled();
+
+    static DelegatedAccessGrant extractDelegatedAccessGrant(String body) {
+        final Map<String, String> response = Splitter.on('&').trimResults().withKeyValueSeparator("=").split(body)
+                .entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, e -> urlDecode(e.getValue())));
+
+        final String code = response.get("code");
+        final PartnerCenterJWT partnerJWT = PartnerCenterJWT.fromTokenString(response.get("id_token"));
+
+        String state = response.get("state");
+        return new DelegatedAccessGrant(code, state, partnerJWT.getTid());
+    }
+
+    static String urlDecode(String param) {
+        try {
+            return URLDecoder.decode(param, Charsets.UTF_8.toString());
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
