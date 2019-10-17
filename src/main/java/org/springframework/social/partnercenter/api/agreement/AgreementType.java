6668c0515460470f888d3b5749333a7824401490
AButelman <andres.butelman@appdirect.com>
2019-10-17 11:31:13 -0300

    Added templateId in AgreementType enum

--- src/main/java/org/springframework/social/partnercenter/api/agreement/AgreementType.java
+++ src/main/java/org/springframework/social/partnercenter/api/agreement/AgreementType.java
@@ -1,19 +1,14 @@
 package org.springframework.social.partnercenter.api.agreement;
 
-import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@AllArgsConstructor
+@Getter
 public enum AgreementType {
-	MICROSOFT_CLOUD_AGREEMENT("MicrosoftCloudAgreement"),
-	MICROSOFT_CUSTOMER_AGREEMENT("MicrosoftCustomerAgreement");
+	MICROSOFT_CLOUD_AGREEMENT("MicrosoftCloudAgreement", "998b88de-aa99-4388-a42c-1b3517d49490"),
+	MICROSOFT_CUSTOMER_AGREEMENT("MicrosoftCustomerAgreement", "117a77b0-9360-443b-8795-c6dedc750cf9");
 
 	private String jsonValue;
-
-	AgreementType(String jsonValue) {
-		this.jsonValue = jsonValue;
-	}
-
-	@JsonValue
-	public String jsonValue() {
-		return jsonValue;
-	}
+	private String templateId;
 }
