ea92076db9df9d26a7debf3c36cd6f4c9f00416c
AButelman <andres.butelman@appdirect.com>
2019-10-17 11:44:23 -0300

    Added getters manually, as using Lombok breaks previous usages.

--- src/main/java/org/springframework/social/partnercenter/api/agreement/AgreementType.java
+++ src/main/java/org/springframework/social/partnercenter/api/agreement/AgreementType.java
@@ -1,14 +1,18 @@
 package org.springframework.social.partnercenter.api.agreement;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+
+import com.fasterxml.jackson.annotation.JsonValue;
 
 @AllArgsConstructor
-@Getter
 public enum AgreementType {
 	MICROSOFT_CLOUD_AGREEMENT("MicrosoftCloudAgreement", "998b88de-aa99-4388-a42c-1b3517d49490"),
 	MICROSOFT_CUSTOMER_AGREEMENT("MicrosoftCustomerAgreement", "117a77b0-9360-443b-8795-c6dedc750cf9");
 
 	private String jsonValue;
 	private String templateId;
+	
+	@JsonValue
+	public String jsonValue() { return jsonValue; }
+	public String getTemplateId() { return templateId; }
 }
