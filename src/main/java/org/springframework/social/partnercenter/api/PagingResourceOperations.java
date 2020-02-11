fc5fd2efa8a19823555f4dc117e3d49bc0919bd4
Bhupendra Singh <bhupendra.singh@appdirect.com>
2020-02-11 23:13:57 +0530

    release/major/add-next-method : Added new `next` method for seek_operation=Next for Azure Utilization API

--- src/main/java/org/springframework/social/partnercenter/api/PagingResourceOperations.java
+++ src/main/java/org/springframework/social/partnercenter/api/PagingResourceOperations.java
@@ -1,6 +1,7 @@
 package org.springframework.social.partnercenter.api;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.social.partnercenter.api.billing.usage.UtilizationRecord;
 
 public interface PagingResourceOperations<T> {
 	ResponseEntity<PartnerCenterResponse<T>> next(String continuationToken);
@@ -8,4 +9,5 @@ public interface PagingResourceOperations<T> {
 	ResponseEntity<PartnerCenterResponse<T>> first(String continuationToken);
 	ResponseEntity<PartnerCenterResponse<T>> last(String continuationToken);
 	ResponseEntity<PartnerCenterResponse<T>> page(String continuationToken, int pageIndex);
+	ResponseEntity<PartnerCenterResponse<UtilizationRecord>> next(String continuationToken,String customerId, String subscriptionId);
 }
