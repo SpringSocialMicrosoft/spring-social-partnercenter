bafae4ea3c0b60344a44384102201638bcfd7ec5
Bhupendra Singh <bhupendra.singh@appdirect.com>
2020-09-22 15:04:20 +0530

    release/minor/update-log-method:Corrected log method

--- src/main/java/org/springframework/social/partnercenter/http/logging/Slf4jHttpRequestResponseLogger.java
+++ src/main/java/org/springframework/social/partnercenter/http/logging/Slf4jHttpRequestResponseLogger.java
@@ -42,7 +42,7 @@ public class Slf4jHttpRequestResponseLogger implements HttpRequestResponseLogger
 			} else if (logger.isInfoEnabled() && logLevel.equals(LogLevel.INFO)){
 				requestLogger.logInfo(startTime, request, body);
 				responseLogger.logInfo(endTime, request, response);
-			} else if (logger.isInfoEnabled() && logLevel.equals(LogLevel.WARN)){
+			} else if (logger.isWarnEnabled() && logLevel.equals(LogLevel.WARN)){
 				requestLogger.logWarning(startTime, request, body);
 				responseLogger.logWarning(endTime, request, response);
 			}
